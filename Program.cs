b756ccf43032b65461813028a6201370ff9da262
OCKx <ockpaul1@gmail.com>
2024-05-14 18:28:05 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the student's score:");
+        int score = Convert.ToInt32(Console.ReadLine());
+
+        char grade = GradeStudent(score);
+
+        Console.WriteLine("Student's grade: " + grade);
+
+        Console.ReadLine();
+    }
+
+    static char GradeStudent(int score)
+    {
+        if (score >= 90)
+        {
+            return 'A';
+        }
+        else if (score >= 80)
+        {
+            return 'B';
+        }
+        else if (score >= 70)
+        {
+            return 'C';
+        }
+        else if (score >= 60)
+        {
+            return 'D';
+        }
+        else
+        {
+            return 'F';
+        }
+    }
+}
